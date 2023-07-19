09f01dc1ab2b6f8dea6dd2a673bf24a02f62d9bc
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-19 21:51:25 +0000

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,11 +6,19 @@ from typing import Union
 
 
 class Cache:
+    """ Creates a Cache class"""
+
     def __init__(self):
+        """ Initializes the Cache instances"""
+
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Generates a random key (e.g using uuid), store the input
+            data in Redis using the random key method and return the key
+        """
+
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
